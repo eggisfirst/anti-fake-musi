69d453fb63e3b57343d072aff40c45587c59c3da
mangoguang <542436458@qq.com>
2018-12-02 11:32:20 +0800

    公众号扫描成功。

--- src/pages/home.js
+++ src/pages/home.js
@@ -17,11 +17,45 @@ class Home extends Component {
     this.rulesClickIn = () => {
       this.setState({ isToggleOn: false })
     }
+    this.scanQr = (res) => {
+      // let result = 'https://zs.derucci.net/service/index.html?state=1&barCode=005056801F5E1ED8AEF55BA4C684CE18'
+      let result = res.resultStr
+      let isUrl = Variable.isDerucci(result) || Variable.isZsDerucci(result)
+      if(!isUrl){
+      //1.20-50的字符串且不是我们的网址(20～50的字符串指的是防伪码，不包含网址。)
+        if (result.length >=20){
+          this.props.history.push('/' + '?barCode=' + result)
+        }else{
+          alert('该二维码不是防伪码')
+        } 
+      }else{
+        //2.是我们的网址
+        if (Variable.isDerucci(result)){
+          let code = Variable.GetQueryString('b',result)
+          console.log('62防伪码', result)
+          //3.有参数
+          if (code.length !== 0){
+            this.props.history.push('/' + '?barCode=' + code)
+          }else {
+            this.props.history.push('/' + '?barCode=')
+          }
+        }else if(Variable.isZsDerucci(result)){
+          console.log('斩杀个防伪码', result)
+          let code2 = Variable.GetQueryString('barCode',result)
+          if (code2.length !== 0){
+            this.props.history.push('/' + '?barCode=' + code2)
+          }else {
+            this.props.history.push('/' + '?barCode=')
+          }
+        } else{
+          alert ('该二维码不是防伪码')
+        }
+      }
+    }
     //微信配置
     this.wxConfig = () => {
       Variable.getTicket()
         .then(function (res) {
-          console.log(res)
           wx.config({
             debug: false,
             appId: 'wx877a7e37b0de0a87',
@@ -32,46 +66,18 @@ class Home extends Component {
           })
         })
         .catch(function (error) {
-          console.log('error', error)
         })
     }
     //调用扫一扫
     this.scanCode = () => {
       console.log('调用扫一扫')
+      let _this = this
+        _this.scanQr('res')
       wx.scanQRCode({
         needResult: 1,
         scanType: ['qrCode', 'barCode'],
         success: (res) => {
-          let result = res.resultStr
-          let isUrl = Variable.isDerucci(result) || Variable.isZsDerucci(result)
-          if(!isUrl){
-          //1.20-50的字符串且不是我们的网址
-            if (result.length >=20 && result.length <=50){
-              this.props.history.push('/' + '?barCode=' + result)
-            }else{
-              alert('该二维码不是防伪码')
-            } 
-          }else{
-            //2.是我们的网址
-            if (Variable.isDerucci(result)){
-              let code = Variable.GetQueryString('b',result) 
-              //3.有参数
-              if (code.length !== 0){
-                this.props.history.push('/' + '?barCode=' + code)
-              }else {
-                this.props.history.push('/' + '?barCode=')
-              }
-            }else if(Variable.isZsDerucci(result)){
-              let code2 = Variable.GetQueryString('barCode',result)
-              if (code2.length !== 0){
-                this.props.history.push('/' + '?barCode=' + code2)
-              }else {
-                this.props.history.push('/' + '?barCode=')
-              }
-            } else{
-              alert ('该二维码不是防伪码')
-            }
-          }
+          _this.scanQr(res)
         }
       })
     }
